b79afd420628c573b26bb8ad761de688a806e8e3
patriciarealini <hello@patriciarealini.com>
2016-04-16 23:07:43 -0700

    first steps for the hamming for loops

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,5 +1,15 @@
 var hamming = function(pairOne, pairTwo) {
 
+  var ham = 0;
+
+  for (x = 0, y= 0; x <= pairOne.length, y<= pairTwo.length ;x++, y++) {
+    if (x != y) {
+      ham++;
+    } 
+  }
+
+  return ham;
+
 };
 
 module.exports = hamming;
